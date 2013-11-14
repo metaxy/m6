39700f8259c98a00fa3dd164a08eb731be267b1b
Paul Walger <metaxy@walger.name>
2013-11-14 21:25:05 +0100

    sermonlist

--- Handler/SermonsList.hs
+++ Handler/SermonsList.hs
@@ -1,12 +1,20 @@
 module Handler.SermonsList where
 
 import Import
+{--formatSpeaker :: Maybe SermonSpeakerId -> Widget
+formatSpeaker (Just x) = do
+    a <- getBy $ UniqueGroupAlias "test"
+    toWidget $ [hamlet| <a> |]
 
+formatSpeaker _ = do
+    toWidget $ [hamlet| <a> |]
+    --}    
 getSermonsListR :: Text -> Text -> Handler Html
 getSermonsListR cat grp = do
-    groupId <- runDB $ getBy404 $ UnqiueGroupAlias grp
+    groupId <- fmap entityKey $ runDB $ getBy404 $ UniqueGroupAlias grp
     sermons <- runDB $ selectList [SermonSermonGroupId ==. groupId] []
     defaultLayout $ do $(widgetFile "SermonList")
-    
+
+
 postSermonsListR :: Text -> Text -> Handler Html
 postSermonsListR = error "Not yet implemented: postSermonsListR"
